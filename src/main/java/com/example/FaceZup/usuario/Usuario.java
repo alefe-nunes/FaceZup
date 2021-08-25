370feed88f34c7fb2e9425bf71562f14e4426d2b
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-25 17:00:16 -0300

    Implementar model usuario de acordo com o padrão bin

--- src/main/java/com/example/FaceZup/usuario/Usuario.java
+++ src/main/java/com/example/FaceZup/usuario/Usuario.java
@@ -1,2 +1,51 @@
-package com.example.FaceZup.usuario;public class Usuario {
+package com.example.FaceZup.usuario;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "usuarios")
+public class Usuario {
+
+    private String nome;
+    private String sobrenome;
+    @Id
+    private String email;
+    private String cargo;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
