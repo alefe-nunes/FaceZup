40c87125ab69247ede0e39e9f08d6a24e202e531
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-25 18:08:19 -0300

    Implementar Usuario Controller

--- src/main/java/com/example/FaceZup/usuario/UsuarioService.java
+++ src/main/java/com/example/FaceZup/usuario/UsuarioService.java
@@ -1,4 +1,12 @@
 package com.example.FaceZup.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
 }
