83f33f337370e243a4e80b41cd47b5e9ab3e97ff
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-25 22:06:31 -0300

    Implantar model de mensagem

---
+++ src/main/java/com/example/FaceZup/chat/Mensagem.java
@@ -0,0 +1,76 @@
+package com.example.FaceZup.chat;
+
+import com.example.FaceZup.usuario.Usuario;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "chat")
+public class Mensagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Integer id;
+    private String mensagem;
+    private Usuario destino;
+    private Usuario origem;
+    private LocalDate dataDeEnvio;
+    private boolean visualizado;
+
+    @ManyToOne
+    @JoinColumn (name = "usuario_origem_email", nullable = false)
+    @JoinColumn (name = "usuario_destino_email" , nullable = false)
+    private Usuario usuario;
+
+    public Mensagem() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public Usuario getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Usuario destino) {
+        this.destino = destino;
+    }
+
+    public Usuario getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(Usuario origem) {
+        this.origem = origem;
+    }
+
+    public LocalDate getDataDeEnvio() {
+        return dataDeEnvio;
+    }
+
+    public void setDataDeEnvio(LocalDate dataDeEnvio) {
+        this.dataDeEnvio = dataDeEnvio;
+    }
+
+    public boolean isVisualizado() {
+        return visualizado;
+    }
+
+    public void setVisualizado(boolean visualizado) {
+        this.visualizado = visualizado;
+    }
+}
